ddd9ccb7a4a132e5fbbe2ddc9fd77e1e56cd40eb
Luan Gabriel Hayakawa Duarte <luan.duarte@joindeveloper.com.br>
2025-10-09 13:58:09 -0300

    Comentando db:generate e db:deploy 2

--- Docker/scripts/generate_database.sh
+++ Docker/scripts/generate_database.sh
@@ -10,13 +10,13 @@ if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ||
     export DATABASE_URL
     echo "Generating database for $DATABASE_PROVIDER"
     echo "Database URL: $DATABASE_URL"
-    npm run db:generate
-    if [ $? -ne 0 ]; then
-        echo "Prisma generate failed"
-        exit 1
-    else
-        echo "Prisma generate succeeded"
-    fi
+    #npm run db:generate
+    # if [ $? -ne 0 ]; then
+    #     echo "Prisma generate failed"
+    #     exit 1
+    # else
+    #     echo "Prisma generate succeeded"
+    # fi
 else
     echo "Error: Database provider $DATABASE_PROVIDER invalid."
     exit 1
